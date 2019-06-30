56bb96fdeab136394702f3ca948f525c2491ebe8
Rahul Akkem <rra42@drexel.edu>
2019-06-29 22:13:56 -0400

    adding Primer gues game to Module02

--- Module02/guess_primer.py
+++ Module02/guess_primer.py
@@ -1,39 +1,32 @@
-
-import random
-
+import random # imports the random library for use later
 print('---------------------------------')
-print('   GUESS THAT PRIMER  GAME')
+print('   GUESS THAT PRIMER  GAME') # gives the name of the game
 print('---------------------------------')
 print()
-
 #GOAL= random.randint(0, 100)
-GOAL=random.choice('ACGT')
+GOAL=random.choice('ACGT') # creates the first letter for the random primer with letter of A,G,C, and\ or T
+GOAL+=random.choice('ACGT') # next for lines subsequently add on to the primer to create a random primer
 GOAL+=random.choice('ACGT')
 GOAL+=random.choice('ACGT')
 GOAL+=random.choice('ACGT')
-GOAL+=random.choice('ACGT')
-
-
-guess = 'NNNNN'
-
-name = input('Player what is your name? ')
-
-while guess != GOAL:
-    guess = input('Guess a 5 base primer containing letters of A,G,C,T: ')
-
-    misses=0
-    for i in range(len(guess)):
+guess = 'NNNNN' # initial dummy guess string
+name = input('Player what is your name? ') # asks for players name
+forcedquit='Quit' # gives the player the option to force quit
+while guess != GOAL: # while loop to run until guess from the user equals the random primer generated
+    guess = input('Guess a 5 base primer containing letters of A,G,C,T: ') # guess taken as an input from the user
+    if guess==forcedquit:
+        print('You quit. The primer is %s' %GOAL)
+        break
+    misses=0 #number of wrong letters counter
+    for i in range(len(guess)): # for loop to check how many misses are between guess and random primer generated
         if guess[i]!=GOAL[i]:
             misses+=1
 
     if misses>0:
-        print('sorry, you guuessed %i bases wrong.Play again' % misses)
+        print('sorry, you guuessed %i bases wrong.Play again. Or type "Quit" to force quit and output the primer' % misses)
+        # allows for retry if more than 1 wrong
     else:
-        print('%s you got the correct primer: %s'% name,% guess)
-
-
-
-
+        print('%s you got the correct primer: %s' % (name , guess)) # outputs persons name and the correct primer
     #if guess != GOAL:
         # print('Your guess of ' + guess + ' was too LOW.')
      #   print('Sorry {}, your guess of {} was too LOW.'.format(name, guess))

fdc77a4aeffd664a46d225f969e50ab50ed529b9
Rahul Akkem <rra42@drexel.edu>
2019-07-16 15:17:32 -0400

    remove this file

--- Module06/wizard_code.py
+++ Module06/wizard_code.py
@@ -1,60 +1,54 @@
-import random
+import random #imports random library for random dice roll
 
-class creature(object):
-   def __init__(self,name,level):
-      self.name=name
-      self.level=level
-   def print(self):      # this is a method which is a function attached to an object
-        print('Name:', self.name,'Level:', self.level)
-
-def print_creature(creature):
-    print('Name;',creature.name,'Level:',creature.level)
-def attack_roll(self):
-    die=random.randint(1,20)
-    return die*self.level
-def defense_roll(self):
-    die =random.randint(1,20)
-    return die*self.level
-
-class Wizard(creature): # subclass creature so behind the scenes whatever the creature can do then the #creature can also do it
-    def __init__(self,name,level,typ=None):# the type is to know which type the object is and is called overloading where the init in the wizard class override the one in creature so this init will be used
-      self.name=name
-      self.level=level
-      self.typ=typ
+class Creature(object): # creates class of creatures
+    def __init__(self, name, level): # takes in slef and name of the creature as well as level
+        self.name = name# initializes name of the object
+        self.level = level# initializes level of the object
     def print(self):
-        print('Name:',self.name,'Level:',self.level,'Type:',self.typ)
-    def level_up(self):
-      self.level+=1
+        print('Name:', self.name, 'Level:', self.level) # prints out the name of the creature when called upon
+    def print_creature(Creature):# used in upstream classes for printing their creature
+        print('Name;', Creature.name, 'Level:', Creature.level)
+    def attack_roll(self): # attack roll of the creature
+        die = random.randint(1, 20) # attack vlaue
+        return die * self.level # return attack value scaled to level
+    def defense_roll(self): # defense value of the creature
+        die = random.randint(1, 20) # defense value
+        return die * self.level # return scaled value of the defense of creature
+
+class Wizard(Creature): # imports all methods defined in the creature class
+    def __init__(self, name, level, typ=None): # overrides creature class method to allow for making type of the object
+        self.name = name # initializes name of the object
+        self.level = level# initializes level of the object
+        self.typ = typ# initializes type of the object
+    def print(self): # again made to allow for printing of name type and level
+        print('Name:', self.name, 'Level:', self.level, 'Type:', self.typ)
+
+    def level_up(self): # allows user, whicheve class they are other than creature to level up
+        self.level += 1 # increases user level
+#All subsequent classes import same methods as the Wizard but change the type of their object
 
 class IceWizard(Wizard):
     def __init__(self, name, level, typ='ice'):
         self.name = name
         self.level = level
         self.typ = typ
-
 class FireWizard(Wizard):
     def __init__(self, name, level, typ='fire'):
         self.name = name
         self.level = level
         self.typ = typ
-
 class ELF(Wizard):
     def __init__(self, name, level, typ='ELF'):
         self.name = name
         self.level = level
         self.typ = typ
-
 class ORCS(Wizard):
     def __init__(self, name, level, typ='ORC'):
         self.name = name
         self.level = level
         self.typ = typ
-
 class Dwarf(Wizard):
     def __init__(self, name, level, typ='Dwarf'):
         self.name = name
         self.level = level
         self.typ = typ
-
-
-
